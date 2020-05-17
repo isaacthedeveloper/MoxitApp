5435a1a37f2f5612381519ae53dd940e649f75eb
Isaac Ballas <Isaacthedeveloper@gmail.com>
2020-05-16 21:07:21 -0400

    initial commit

---
+++ MoxitApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MoxitApp
+//
+//  Created by Isaac Ballas on 5/16/20.
+//  Copyright © 2020 Isaac Ballas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
